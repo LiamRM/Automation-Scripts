e74d70fafac005475df25b6494436732d19c5196
LiamRM <lrm444@nyu.edu>
2021-10-30 17:03:26 -0400

    :art: prettier output

--- sortDownloads.py
+++ sortDownloads.py
@@ -17,21 +17,27 @@ p = Path.home()
 # -- the format is {abbreviation: name of class / folder to be stored in}
 classes = {"DT_": "Downtown Theater", "CS_": "Computer Security"}
 
+# Counters
+mvctr = 0
+delctr = 0
+
 ###         Create folders to each class if DNE      ###
 for className in classes.values():
     if (p / "Desktop" / className).exists() == False:
         print("Folder does not exist. Making folder now.")
         (p / "Desktop" / className).mkdir()
 
-downloadFiles = os.listdir(p / "Downloads")       # returns list of all files in Downloads dir
+###         Sort files in Downloads         ###
+downloadFiles = os.listdir(p / "Downloads")             # returns list of all files in Downloads dir
 for filename in downloadFiles:
-    if filename[:3] in classes.keys():                   # Find every file beginning with class abbreviation (Ex: "DT_")
+    if filename[:3] in classes.keys():                  # Find every file beginning with class abbreviation (Ex: "DT_")
         src = p / "Downloads" / filename
         dest = p / "Desktop" / classes[filename[:3]]
-        #shutil.move(src, dest)
-        print(src, dest)
-
+        shutil.move(src, dest)
+        mvctr += 1
 
 ### Send all setup / installer files to the trash
 # send2trash module (3rd party, installed via 'pip install send2trash')
 # send2trash.send2trash("filename")
+
+print(f"{mvctr} files moved, {delctr} files deleted.")
