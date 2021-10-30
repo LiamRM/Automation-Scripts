6c9c5fd9e758c6caccd3062d4a6223dbd6749a7a
LiamRM <lrm444@nyu.edu>
2021-10-30 16:56:14 -0400

    Added script to sort Downloads folder

---
+++ sortDownloads.py
@@ -0,0 +1,37 @@
+"""
+Name        : sortDownloads.py
+Author      : Liam Richards
+Date created: Oct 30th, 2021
+Description : This script will sort through my Downloads folder for me on my Windows 10 PC.
+              This is a task I wind up doing more often than I'd like as I download texts for class
+              and continuously move them to the same folders.
+"""
+
+import shutil, os           # shutil --> to copy, move, rename, and delete files
+from pathlib import Path    # Path to working directory
+
+p = Path.home() 
+
+# Variables 
+# -- these abbreviations MUST be unique and 3 characters long!
+# -- the format is {abbreviation: name of class / folder to be stored in}
+classes = {"DT_": "Downtown Theater", "CS_": "Computer Security"}
+
+###         Create folders to each class if DNE      ###
+for className in classes.values():
+    if (p / "Desktop" / className).exists() == False:
+        print("Folder does not exist. Making folder now.")
+        (p / "Desktop" / className).mkdir()
+
+downloadFiles = os.listdir(p / "Downloads")       # returns list of all files in Downloads dir
+for filename in downloadFiles:
+    if filename[:3] in classes.keys():                   # Find every file beginning with class abbreviation (Ex: "DT_")
+        src = p / "Downloads" / filename
+        dest = p / "Desktop" / classes[filename[:3]]
+        #shutil.move(src, dest)
+        print(src, dest)
+
+
+### Send all setup / installer files to the trash
+# send2trash module (3rd party, installed via 'pip install send2trash')
+# send2trash.send2trash("filename")
