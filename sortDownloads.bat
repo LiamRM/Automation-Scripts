0c4df7b44bc0ad1414c5906daac5437305f79090
LiamRM <lrm444@nyu.edu>
2021-10-30 17:03:45 -0400

    created batch file for easy launching

---
+++ sortDownloads.bat
@@ -0,0 +1,5 @@
+:: This Batch file will run the sortDownloads.py simply by typing "Win + R" then "sortDownloads"
+@py.exe "C:/Users/liami/Automation Scripts/sortDownloads.py"
+
+:: Adds "Press any key to continue...", prevents window from disappearing too quickly
+@pause
