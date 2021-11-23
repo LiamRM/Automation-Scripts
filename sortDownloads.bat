5f122997ae771e33b55b63f90b0723866140769a
LiamRM <lrm444@nyu.edu>
2021-11-23 10:20:39 -0500

    Scheduled the sortDownloads task on Windows

--- sortDownloads.bat
+++ sortDownloads.bat
@@ -3,3 +3,7 @@
 
 :: Adds "Press any key to continue...", prevents window from disappearing too quickly
 @pause
+
+:: Scheduling Tasks
+:: EX, 'security script' to run every 20 mins: schtasks /create /sc minute /mo 20 /tn "Security Script" /tr \\central\data\scripts\sec.vbs
+:: To delete a scheduled task: schtasks /delete /tn {TaskName | *} 
